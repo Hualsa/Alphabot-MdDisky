5833e8be35e20c7cc47f1030a402c561c4d23a96
Hualsa <107768014+Hualsa@users.noreply.github.com>
2022-06-23 22:09:08 +0800

    Add files via upload

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const chalk = require('chalk')
 
 // Website Api
 global.APIs = {
-	alfa: 'https://api.zeeoneofc.xyz', //apabila link api eror, segera laporkan le owner
+	alfa: 'https://api.zeeoneofc.xyz', //apabila link api eror, segera laporkan ke owner
 }
 
 // Free apikey
@@ -12,27 +12,27 @@ global.APIKeys = {
 }
 
 // setting 
-global.autoread = false // auto read pesan / message
+global.autoread = true // auto read pesan / message
 //global.autorecording = true //status auto merekam ( auto record )
 //global.autoketik = false //status auto mengetik (auto typing)
 //global.available = false //status online (online)
 
 // Other
-global.botname = "ᴹᴿ᭄DarkStalker BOT😈ོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ Luck Xxyོ ×፝֟͜×" //nama kalian
-global.myweb ="https://discord.gg/m4eRAs8Zf2" //bebas asal jan hapus
-global.youtube = "https://youtube.com/channel/UC5jaRLAdMkgxoLf8t9N37dw" //bebas asal jan hapus
-global.github = "GA ADA GITHUB OI" //bebas
-global.email = "ProDarkStalkerBOT@gmail.com" //bebas
+global.botname = "PRObot~OMskyོ" //namabot kalian
+global.ownername= "ᴹᴿ᭄ OmSkyོ ×፝֟͜×" //nama kalian
+global.myweb ="No" //bebas asal jan hapus
+global.youtube = "NO" //bebas asal jan hapus
+global.github = "no" //bebas
+global.email = "BOT OmSky@gmail.com" //bebas
 global.region = "Indonesia" //bebas
-global.ownernomer = "6285904142544" // nomor wa kalian
-global.ownernomerr = "+6285904142544" //nmr wa kalian
+global.ownernomer = "62895705000123" // nomor wa kalian
+global.ownernomerr = "+62895705000123" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ['6285904142544','6283824566640','628895082547','62881027973711','6289517158630','6285848171414'] //ganti agar fitur owner bisa di gunakan
-global.packname = '© DarkStalker BOTོ' //sticker wm ubah
-global.author = 'Di Buat Oleh Luck Xxy' //sticker wm ganti nama kalian
+global.owner = ["t62895705000123","6285904142544"] //ganti agar fitur owner bisa di gunakan
+global.packname = '© PRObot~OMskyོ-ོ' //sticker wm ubah
+global.author = 'Di Buat Oleh OmSky Bot' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
